b64cee9a8b283d0fcc41da1ee89171481cb574c3
Sumeet Agarwal <sumeet.a@gmail.com>
2023-05-27 21:10:35 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rand;
 use rand::prelude::SliceRandom;
 use shakmaty::uci::Uci;
-use shakmaty::{Chess, Color, Move, Position, Role};
-use std::io::Write;
+use shakmaty::{Chess, Color, Move, Outcome, Position, Role};
+use std::fs::File;
 use std::io::{self, BufRead};
+use std::io::{BufReader, Write};
+use std::num::{NonZeroU32, NonZeroU8};
 use std::process::exit;
 use std::str::FromStr;
 use vampirc_uci::{parse_one, UciMessage, UciSquare};
@@ -36,7 +38,103 @@ impl State {
     }
 }
 
+use pgn_reader::{BufferedReader, SanPlus, Visitor};
+use shakmaty::san::San;
+
+struct GameCollector {
+    current_pos: Chess,
+    moves: Vec<Move>,
+    num_games_collected: usize,
+    output: File,
+}
+
+impl GameCollector {
+    fn new() -> Self {
+        Self {
+            moves: vec![],
+            current_pos: Chess::default(),
+            num_games_collected: 0,
+            output: File::create("output.chess").unwrap(),
+        }
+    }
+}
+
+enum ParseResult {
+    KeepGoing,
+    Done,
+}
+
+impl Visitor for GameCollector {
+    type Result = ParseResult;
+
+    fn san(&mut self, san: SanPlus) {
+        let m = San::from_str(&format!("{}", san))
+            .unwrap()
+            .to_move(&self.current_pos)
+            .unwrap();
+        self.current_pos = self.current_pos.clone().play(&m).unwrap();
+        self.moves.push(m);
+    }
+
+    fn end_game(&mut self) -> Self::Result {
+        use ParseResult::*;
+
+        let mut end_pos = Chess::default();
+        std::mem::swap(&mut end_pos, &mut self.current_pos);
+
+        let mut end_moves = vec![];
+        std::mem::swap(&mut end_moves, &mut self.moves);
+
+        if end_pos.fullmoves() > unsafe { NonZeroU32::new_unchecked(50) } {
+            return KeepGoing;
+        }
+
+        let outcome = end_pos.outcome();
+        // for now also filter out draws because we only want to choose
+        // moves from the winner
+        if let None | Some(Outcome::Draw) = outcome {
+            return KeepGoing;
+        }
+        let outcome = outcome.unwrap();
+
+        let (last_move, moves) = end_moves.split_last().unwrap();
+        for m in moves {
+            write!(self.output, "{},", Uci::from_standard(&m)).unwrap();
+        }
+        write!(self.output, "{}\n", Uci::from_standard(&last_move)).unwrap();
+
+        match outcome {
+            Outcome::Decisive { winner } => match winner {
+                Color::Black => write!(self.output, "B\n").unwrap(),
+                Color::White => write!(self.output, "W\n").unwrap(),
+            },
+            Outcome::Draw => write!(self.output, "D\n").unwrap(),
+        }
+        self.num_games_collected += 1;
+        KeepGoing
+        // if self.num_games_collected > 200_000 {
+        //     Done
+        // } else {
+        //     KeepGoing
+        // }
+    }
+}
+
 fn main() {
+    let f = File::open("./download/elite.pgn.gz").unwrap();
+    let reader = BufReader::new(f);
+    let reader = flate2::bufread::GzDecoder::new(reader);
+    let mut pgn_reader = BufferedReader::new(reader);
+    let mut game_collector = GameCollector::new();
+    while let Some(parse_result) = pgn_reader.read_game(&mut game_collector).unwrap() {
+        match parse_result {
+            ParseResult::KeepGoing => {}
+            ParseResult::Done => break,
+        }
+    }
+}
+
+fn main2() {
     let mut state = State::init();
     let mut log_file = std::fs::File::create("/tmp/log.txt").unwrap();
     let mut rng = rand::thread_rng();
