c00d705914b2a0ea1c9edcbb74d424f58676d9e8
Sumeet Agarwal <sumeet.a@gmail.com>
2023-06-12 21:44:47 -0700

    extract training stuff from google colab

---
+++ train/inputs.py
@@ -0,0 +1,218 @@
+# input:
+#     [board position]
+import gc
+import multiprocessing
+from itertools import islice, chain
+import h5py
+
+# output: (basically a move)
+#     - src square
+#     - dst square
+#     - piece (rook, pawn, queen, etc.)
+
+
+# training method:
+#     - games from 2500+ rated players
+#     - having the model try to learn moves made by the winner
+
+
+import chess
+import numpy as np
+import gzip
+
+PIECES_ORDER = [
+    "pawn",
+    "knight",
+    "bishop",
+    "rook",
+    "queen",
+    "king",
+]  # Chess piece types
+PIECES = {name: i for i, name in enumerate(PIECES_ORDER)}
+
+
+def board_to_tensors(board):
+    tensor_board = np.zeros((12, 8, 8), dtype=np.float16)
+
+    # Loop through all squares on the board
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece:
+            # Get piece type and color
+            piece_type = chess.piece_name(piece.piece_type)
+            color = piece.color
+
+            # Calculate the coordinates
+            row = square // 8
+            col = square % 8
+
+            # Calculate the depth index based on the piece type and color
+            piece_index = PIECES[piece_type] + (6 if color == chess.BLACK else 0)
+
+            # Place the piece in the tensor
+            tensor_board[piece_index, row, col] = 1
+
+    tensor_extras = np.zeros(7, dtype=np.float16)
+    # Append castling rights
+    tensor_extras[0] = int(board.has_kingside_castling_rights(chess.WHITE))
+    tensor_extras[1] = int(board.has_queenside_castling_rights(chess.WHITE))
+    tensor_extras[2] = int(board.has_kingside_castling_rights(chess.BLACK))
+    tensor_extras[3] = int(board.has_queenside_castling_rights(chess.BLACK))
+
+    # Append en passant square
+    if board.ep_square is not None:
+        tensor_extras[4] = board.ep_square
+    else:
+        tensor_extras[4] = 64
+
+    # Append white's turn
+    tensor_extras[5] = 1 if board.turn == chess.WHITE else 0
+
+    # Append is in check
+    tensor_extras[6] = 0 if board.is_check() else 1
+
+    return (tensor_board, tensor_extras)
+
+
+cache = {}
+for i, src_file in enumerate("abcdefgh"):
+    for j, src_rank in enumerate("12345678"):
+        for k, dst_file in enumerate("abcdefgh"):
+            for l, dst_rank in enumerate("12345678"):
+                for piece_name in PIECES_ORDER:
+                    # 128 for the board (8*8*2), 6 for the piece
+                    t = np.zeros(128 + 6, dtype=np.float16)
+                    src_index = i * 8 + j
+                    dst_index = k * 8 + l
+                    src_move = src_file + src_rank
+                    dst_move = dst_file + dst_rank
+                    t[src_index] = 1.0
+                    t[64 + dst_index] = 1.0
+                    piece_index = PIECES[piece_name]
+                    t[128 + piece_index] = 1.0
+                    cache[src_move + dst_move + piece_name] = t
+
+
+def move_to_tensor(move: chess.Move, board_after: chess.Board):
+    piece_name = chess.piece_name(board_after.piece_type_at(move.to_square))
+    return cache[move.uci()[:4] + piece_name]
+
+
+# cache = {}
+# for i, src_file in enumerate('abcdefgh'):
+#     for j, src_rank in enumerate('12345678'):
+#         for k, dst_file in enumerate('abcdefgh'):
+#             for l, dst_rank in enumerate('12345678'):
+#                 for piece_name in PIECES_ORDER:
+#                     piece = PIECES[piece_name]
+#                     # 2 for the board, 1 for the piece
+#                     t = torch.zeros(3, dtype=torch.float16)
+#                     t[0] = i*8+j
+#                     t[1] = k*8+l
+#                     t[2] = PIECES[piece_name]
+#                     cache[src_file+src_rank+dst_file+dst_rank+piece_name] = t
+
+# def move_to_tensor(move: chess.Move, board_after: chess.Board):
+#    piece_name = chess.piece_name(board_after.piece_type_at(move.to_square))
+#    return cache[move.uci()[:4] + piece_name]
+
+def process_line_chunk(line_chunk):
+    i, line_chunk = line_chunk
+
+    input_board = []
+    input_extras = []
+    output = []
+    for line_pair in line_chunk:
+        boards, extras, outputs = process_line_pair(line_pair)
+        input_board.extend(boards)
+        input_extras.extend(extras)
+        output.extend(outputs)
+    with LOCK:
+        with h5py.File(OUTPUT_H5_FILENAME, "a") as f:
+            [input_board_ds, input_extras_ds, output_ds] = [
+                f[ds_name] for ds_name in ["input_board", "input_extras", "output"]
+            ]
+            current_size = input_board_ds.shape[0]
+            new_size = current_size + len(input_board)
+
+            for ds, data in zip(
+                    [input_board_ds, input_extras_ds, output_ds],
+                    [input_board, input_extras, output]):
+                stacked_data = np.stack(data)
+                ds.resize((new_size,) + stacked_data.shape[1:])
+                ds[current_size:new_size, ...] = stacked_data
+
+    print(f"Processed boards: {i * 10_000}")
+
+
+def process_line_pair(line_pair):
+    input_board_tensors = []
+    input_extras_tensors = []
+    output_tensors = []
+
+    moves, winner = line_pair
+    moves = moves.split(b",")
+    winner = winner.strip()
+
+    if winner == b"D":
+        return [], [], []
+
+    to_move = b"W"
+
+    board = chess.Board()
+    for i, move_san in enumerate(moves):
+        move_san = move_san.strip()
+
+        use_as_input = to_move == winner and i < len(moves)
+
+        if use_as_input:
+            b, e = board_to_tensors(board)
+            input_board_tensors.append(b)
+            input_extras_tensors.append(e)
+
+        move = board.push_san(move_san.decode("utf-8"))
+
+        if use_as_input:
+            o = move_to_tensor(move, board)
+            output_tensors.append(o)
+
+        to_move = b"W" if to_move == b"B" else b"B"
+
+    return input_board_tensors, input_extras_tensors, output_tensors
+
+
+LOCK = multiprocessing.Lock()
+
+
+def chunks(iterable, chunk_size):
+    it = iter(iterable)
+    while c := list(islice(it, chunk_size)):
+        yield c
+
+
+OUTPUT_H5_FILENAME = "input.h5"
+
+with gzip.open("output.chess.gz", "rb") as f:
+    line_pairs = ((line1, line2) for line1, line2 in zip(f, f))
+    training_size = 200_000
+    line_pairs = islice(line_pairs, training_size)
+
+    with h5py.File(OUTPUT_H5_FILENAME, "w") as f:
+        input_board_ds = f.create_dataset(
+            "input_board", (0, 12, 8, 8), maxshape=(None, 12, 8, 8), dtype="float16", chunks=True, compression="gzip"
+        )
+        input_extras_ds = f.create_dataset(
+            "input_extras", (0, 7), maxshape=(None, 7), dtype="float16", chunks=True, compression="gzip"
+        )
+        output_ds = f.create_dataset(
+            "output", (0, 134), maxshape=(None, 134), dtype="float16", chunks=True, compression="gzip"
+        )
+
+    with multiprocessing.Pool() as p:
+        chunk_size = 5_000
+        pair_chunks = chunks(line_pairs, chunk_size)
+        results = p.imap_unordered(process_line_chunk, enumerate(pair_chunks))
+
+        for i, _ in enumerate(results):
+            print(f"processed {chunk_size * (i + 1)} games")
+
---
+++ train/train.py
@@ -0,0 +1,166 @@
+import torch
+import gzip
+import h5py
+from torch.utils.data import TensorDataset, Dataset, random_split
+
+def split_perc(dataset, percentages):
+    total_data_len = len(dataset)
+    split_lengths = [int(total_data_len * p) for p in percentages]
+    # If there is any difference in sum, assign it to the last length
+    sum_of_lengths = sum(split_lengths)
+    if sum_of_lengths != total_data_len:
+        split_lengths[-1] += total_data_len - sum_of_lengths
+    return random_split(dataset, lengths=split_lengths)
+
+
+if 1:
+    stop = 1_500_000
+
+    with h5py.File('input.h5', "r") as f:
+        input_board_batch = torch.from_numpy(f["input_board"][:stop])
+        input_extras_batch = torch.from_numpy(f["input_extras"][:stop])
+        output_batch = torch.from_numpy(f["output"][:stop])
+
+    dataset = TensorDataset(input_board_batch,
+                            input_extras_batch,
+                            output_batch)
+
+else:
+    class HDF5Dataset(Dataset):
+        def __init__(self, file_path):
+            self.file_path = file_path
+            self.file = h5py.File(self.file_path, "r", rdcc_nslots=1991, rdcc_nbytes=16_000 * (1024 ** 3))
+
+        def __len__(self):
+            return len(self.file["input_board"])
+#            with h5py.File(self.file_path, "r") as file:
+#                return len(file["input_board"])
+
+        def __getitem__(self, idx):
+    #        with h5py.File(self.file_path, "r") as f:
+            input_board = self.file["input_board"][idx]
+            input_extras = self.file["input_extras"][idx]
+            output = self.file["output"][idx]
+            return input_board, input_extras, output
+
+    dataset = HDF5Dataset("./input.h5")
+
+
+import contextlib
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+# output format:
+# [[1/64 src square][1/64 dst square][1/6 piece]]
+
+class ChessMovePredictor(nn.Module):
+    def __init__(self):
+        super(ChessMovePredictor, self).__init__()
+        self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        # 128*8*8 is the size of the flattened conv layer output,
+        # and 7 is the size of the extra features tensor
+        self.fc1 = nn.Linear(128*8*8 + 7, 256)
+        self.fc_res = nn.Linear(12*8*8, 256)  # Transform the original input to the same size as the output of fc1
+        self.relu = nn.ReLU()
+        self.fc_out = nn.Linear(256, 128+6)
+
+    def forward(self, board_tensor, extra_features):
+        out = F.relu(self.conv1(board_tensor))
+        out = F.relu(self.conv2(out))
+        out = out.view(out.size(0), -1)  # Flatten tensor
+        out = torch.cat((out, extra_features), dim=1)  # Concatenate extra features
+        out = self.relu(self.fc1(out))
+        out = out.add(self.fc_res(board_tensor.view(board_tensor.size(0), -1)))  # Transform the original input
+        out = self.fc_out(out)
+        return out
+
+    def run(self, dataloader, is_train):
+        model = self.train() if is_train else self.eval()
+        tot_loss = 0
+        num_iterations = 0
+    
+        ctx = contextlib.nullcontext() if is_train else torch.no_grad()
+        with ctx:
+            for inputs_board, inputs_extras, targets in dataloader:
+                inputs_board = inputs_board.to(device)
+                inputs_extras = inputs_extras.to(device)
+                targets = targets.to(device)
+            
+                # Forward pass
+                with torch.cuda.amp.autocast():
+                    outputs = model(inputs_board, inputs_extras)
+            
+                    targets_source, targets_dest, targets_square = targets[:, :64], targets[:, 64:128], targets[:, 128:]
+                    outputs_source, outputs_dest, outputs_square = outputs[:, :64], outputs[:, 64:128], outputs[:, 128:]
+                    loss_source = loss_fn(outputs_source, targets_source)
+                    loss_dest = loss_fn(outputs_dest, targets_dest)
+                    loss_square = loss_fn(outputs_square, targets_square)
+                    # Compute total loss
+                    loss = loss_source + loss_dest + loss_square
+                    tot_loss += loss
+                    num_iterations += 1
+
+                if is_train:
+                    # Backward pass and optimization
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()    
+    
+        return tot_loss / num_iterations        
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = ChessMovePredictor()
+model = model.to(device)
+#loss_fn = nn.NLLLoss() 
+loss_fn = nn.BCEWithLogitsLoss()
+#loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+from multiprocessing import cpu_count
+from torch.utils.data import DataLoader, TensorDataset
+import time
+
+num_epochs = 10_000
+
+
+training_set, validation_set = split_perc(dataset, [0.8, 0.2])
+
+training_dataloader = DataLoader(training_set,
+                        shuffle=True,
+                        batch_size=10_000,
+                        persistent_workers=True,
+                        num_workers=cpu_count() - 1,
+                        prefetch_factor=50,
+                        pin_memory=True)
+validation_dataloader = DataLoader(validation_set,
+                        shuffle=True,
+                        batch_size=10_000,
+                        persistent_workers=True,
+                        num_workers=cpu_count() - 1,
+                        prefetch_factor=50,
+                        pin_memory=True)
+
+for epoch in range(num_epochs):
+    epoch_start = time.time()
+
+    training_loss = model.run(training_dataloader, is_train=True)
+    validation_loss = model.run(validation_dataloader, is_train=False) 
+
+    if epoch % 5 == 0:
+        filename = 'checkpoint.pt'
+        print(f'checkpointing torch model & optimizer to {filename}')
+        torch.save({
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'training_loss': training_loss,
+            'validation_loss': validation_loss,
+        }, filename)
+
+    time_elapsed = time.time() - epoch_start
+    print('Epoch [{}/{}], Training loss: {:.4f} Validation loss: {:.4f} ({} seconds)'
+        .format(epoch+1, num_epochs, training_loss, validation_loss, time_elapsed))
+
